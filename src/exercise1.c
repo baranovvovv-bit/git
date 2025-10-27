b9411f21704d9c8468e05bd4a13df4d71cded055
baranovvovv <baranovvovv@gmail.com>
2025-10-27 17:35:35 +0700

    fix1

--- src/exercise1.c
+++ src/exercise1.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char** argv) {
-    printf("Hello World!");
+int main(int argc, char** argv)
+{
+    float arr[10];
+    float sum = 0;
+
+    for(int i = 0; i < 10; i++)
+    {
+        scanf("%f", &arr[i]);
+    }
+
+    for(int i = 0; i < 10; i++)
+    {
+        sum += arr[i];
+    }
+    
+    float ans = sum / 10;
+    printf("%.3f\n", ans);
+    
     return 0;
 }
