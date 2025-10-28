78bba67135306d46757dc868f4922c0e806443f3
baranovvovv <baranovvovv@gmail.com>
2025-10-28 12:21:06 +0700

    fix3

--- src/exercise4.c
+++ src/exercise4.c
@@ -1,45 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc,char** argv)
-{
-    int array1[10][3];
-    int array2[3][10];
-    int array_result[10][10];
-
-    for(int i = 0; i < 10;i++)
-    {
-        for(int j = 0; j < 3;j++)
-        {
-            array1[i][j] = 1;
+int main(int argc, char *argv[]) {
+    float arr[10];
+    float x = atof(argv[11]);
+    int k = atoi(argv[12]);
+    for (int i = 0; i < 10; i++) {
+    arr[i] = atoi(argv[i + 1]);
         }
-    }
-
-    for(int i = 0; i < 3; i++)
-    {
-        for(int j = 0 ; j < 10; j++)
-        {
-            array2[i][j] = 2;
+    for (int i = k; i < 10; i++) {
+        printf("%.2f ", arr[i - k]);
         }
-    }
-
-    for(int i = 0; i < 10; i++)
-    {
-        for(int j = 0; j < 10; j++)
-        {
-            array_result[i][j] = 0;
-            for(int k = 0 ; k < 3; k++)
-            {
-                array_result[i][j] += array1[i][k] * array2[k][j];
-            }
-        }
-    }
-
-    for(int i = 0 ; i < 10; i++)
-    {
-        for(int j = 0 ; j < 10; j++)
-        {
-            printf("%d ",array_result[i][j]);
-        }
+    printf("%.2f ", x);
+    for (int i = k; i < 9; i++) {
+        printf("%.2f ", arr[i]);
     }
 }
--- src/exercise5.c
+++ src/exercise5.c
@@ -1,6 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char** argv) {
-    printf("Hello World!");
+int main(int argc, char *argv[]) {
+    double a[10][3]; 
+    double b[3][10];  
+    double f[10][10]; 
+
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 3; j++) {
+            scanf("%lf", &a[i][j]); 
+        }
+    }
+    
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 10; j++) {
+            scanf("%lf", &b[i][j]); 
+        }
+    }
+
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            f[i][j] = 0;
+            for (int k = 0; k < 3; k++) {
+                f[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+    
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            printf("%.2f", f[i][j]);  
+            if (i < 9 || j < 9) printf(" ");
+        }
+    }
+    printf("\n");
+    
     return 0;
 }
