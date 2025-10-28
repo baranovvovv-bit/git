eca5bd903f9b32befe905ae77ba408882712c133
baranovvovv <baranovvovv@gmail.com>
2025-10-28 12:23:49 +0700

    fix4

--- src/exercise4.c
+++ src/exercise4.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 int main(int argc, char *argv[]) {
     float arr[10];
-    float x = atof(argv[11]);
-    int k = atoi(argv[12]);
     for (int i = 0; i < 10; i++) {
-    arr[i] = atoi(argv[i + 1]);
-        }
-    for (int i = k; i < 10; i++) {
-        printf("%.2f ", arr[i - k]);
-        }
+        scanf("%f",&arr[i]);
+    }
+    float x = atof(argv[1]);
+    int k = atoi(argv[2]);
+    for (int i = 0; i < k; i++) {
+        printf("%.2f ", arr[i]);
+    }
     printf("%.2f ", x);
-    for (int i = k; i < 9; i++) {
+    for (int i = k; i < 10; i++) {
         printf("%.2f ", arr[i]);
     }
 }
